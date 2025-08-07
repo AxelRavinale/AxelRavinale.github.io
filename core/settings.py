60bcbf867ee7385f9a786e60ecadab9f3d3efeae
Danilo <Danilo@gavari.com>
2025-08-06 23:09:59 -0300

    Diseño de Vuelo list

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Application definition
 
