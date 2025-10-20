34d2174bf3f6a5b70e9c1173a15c4b7fce783cdd
Danilo <Danilo@gavari.com>
2025-10-19 21:34:32 -0300

    Arreglo reserva

--- vuelos/reportes/views.py
+++ vuelos/reportes/views.py
@@ -6,7 +6,6 @@ from .services import ReporteService
 class ReporteViewSet(viewsets.ViewSet):
     @action(detail=False, methods=['get'], url_path='pasajeros-por-vuelo/(?P<vuelo_id>[^/.]+)')
     def pasajeros_por_vuelo(self, request, vuelo_id=None):
-        """Reporte: lista todos los pasajeros de un vuelo"""
         data = ReporteService.obtener_pasajeros_por_vuelo(vuelo_id)
         if data is None:
             return Response({'error': 'Vuelo no encontrado'}, status=status.HTTP_404_NOT_FOUND)
@@ -14,7 +13,6 @@ class ReporteViewSet(viewsets.ViewSet):
 
     @action(detail=False, methods=['get'], url_path='reservas-activas/(?P<pasajero_id>[^/.]+)')
     def reservas_activas(self, request, pasajero_id=None):
-        """Reporte: reservas activas de un pasajero"""
         data = ReporteService.obtener_reservas_activas_por_pasajero(pasajero_id)
         if data is None:
             return Response({'error': 'Pasajero no encontrado'}, status=status.HTTP_404_NOT_FOUND)
